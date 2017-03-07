a9ea0dbdd6d01fd2bde0d55ef29ffb4d922c1260
gilbero <gilberto_1_0@hotmail.com>
2017-03-06 19:47:14 -0600

    pruebas de archivo imagen

--- src/images/ImagesConverter.java
+++ src/images/ImagesConverter.java
@@ -4,15 +4,27 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.WritableRaster;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
 
 import javax.imageio.ImageIO;
 
 public class ImagesConverter {
 	public static void main(String[] args) {
-		
+		try {
+			byte[] extractBytes = ImagesConverter.extractBytes("D:\\Users\\Senrigan\\Documents\\desarrollo\\CaptchaProject\\captcha\\end-of-captcha.jpg");
+			System.out.println(Arrays.toString(extractBytes));
+			
+			FileOutputStream fos = new FileOutputStream("D:\\Users\\Senrigan\\Documents\\desarrollo\\CaptchaProject\\captcha\\prueba.jpg");
+			fos.write(extractBytes);
+			fos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public byte[] extractBytes (String ImageName) throws IOException {
+	public static  byte[] extractBytes (String ImageName) throws IOException {
 		 // open image
 		 File imgPath = new File(ImageName);
 		 BufferedImage bufferedImage = ImageIO.read(imgPath);
